ce6f21c2c30bfd9b094360f1d9b54c87c9016f7a
Mark Williams <williams_ma@mercer.edu>
2018-06-14 21:39:22 -0400

    Delete everything Before 'muid'

--- NewEmployeeProg.py
+++ NewEmployeeProg.py
@@ -3,15 +3,23 @@
 import pyperclip
 import keyboard
 import tkinter
+import copy
 from pprint import pprint
 
-#
 rawEmail = str(pyperclip.paste())
 splitEmail = rawEmail.split(sep='\n')
 splitEmail = [i.strip() for i in splitEmail]
-listEmail = list(filter(None, splitEmail))
+listEmaila = list(filter(None, splitEmail))
 
-emailDict = dict(zip(listEmail[::2], listEmail[1::2]))  # turn list into dictionary
+listEmailb = copy.copy(listEmaila)
+for line in listEmaila:
+    if line != 'muid':
+        listEmailb.remove(line)
+    else:
+        break
+listEmaila = copy.copy(listEmailb)
+
+emailDict = dict(zip(listEmaila[::2], listEmaila[1::2]))  # turn list into dictionary
 
 print(emailDict)
 # pprint  # pprint for testing
